814e36e5a32f106b251bf17e3aaab8e10e8fffab
CyberCastle <cybercastle@gmail.com>
2025-06-08 16:31:13 -0400

    Update height default in test

--- tests/tripod_gait_sim_test.cpp
+++ tests/tripod_gait_sim_test.cpp
@@ -103,7 +103,7 @@ int main() {
     p.coxa_length = 50;
     p.femur_length = 101;
     p.tibia_length = 208;
-    p.robot_height = 7;
+    p.robot_height = 90;
     p.control_frequency = 50;
     p.coxa_angle_limits[0] = -65;
     p.coxa_angle_limits[1] = 65;
