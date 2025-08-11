15724a4f25c7595b01141aa4422de3364959bc3b
CyberCastle <cybercastle@gmail.com>
2025-08-11 11:50:57 -0400

    Fix c++11 compilation

--- src/leg_stepper.cpp
+++ src/leg_stepper.cpp
@@ -495,7 +495,7 @@ void LegStepper::updateTipPositionIterative(int iteration, double time_delta, bo
             planar_drift_norm_ = std::sqrt(accumulated_drift_vector_.x * accumulated_drift_vector_.x +
                                            accumulated_drift_vector_.y * accumulated_drift_vector_.y);
             vertical_drift_ = accumulated_drift_vector_.z;
-            double alpha = std::clamp(params.drift_metrics_ema_alpha, 0.0, 1.0);
+            double alpha = math_utils::clamp(params.drift_metrics_ema_alpha, 0.0, 1.0);
             if (alpha > 0.0) {
                 drift_ema_norm_ = (1.0 - alpha) * drift_ema_norm_ + alpha * offset_norm;
             } else {
@@ -527,7 +527,7 @@ void LegStepper::updateTipPositionIterative(int iteration, double time_delta, bo
                     current_tip_pose_ = default_tip_pose_;
                     stance_origin_tip_position_ = default_tip_pose_;
                 } else if (soft_reset) {
-                    double alpha = std::clamp(params.drift_soft_blend_alpha, 0.0, 1.0);
+                    double alpha = math_utils::clamp(params.drift_soft_blend_alpha, 0.0, 1.0);
                     current_tip_pose_ = Point3D{current_tip_pose_.x + (default_tip_pose_.x - current_tip_pose_.x) * alpha,
                                                 current_tip_pose_.y + (default_tip_pose_.y - current_tip_pose_.y) * alpha,
                                                 current_tip_pose_.z + (default_tip_pose_.z - current_tip_pose_.z) * alpha};
